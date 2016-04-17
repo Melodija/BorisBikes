6274b26a991334fb4be9da3b2255f1ab7b2f81d5
Ruta <ruta@baltiejus.com>
2016-04-17 11:44:36 +0100

    end of day-four

--- feature_tests.rb
+++ feature_tests.rb
@@ -124,5 +124,3 @@ raise "van didn't pick up correct bikes" unless van.cargo == broken_bikes
 # As a maintainer of the system,
 # So that I can manage broken bikes and not disappoint users,
 # I'd like vans to take broken bikes from docking stations and deliver them to garages to be fixed.
-
-
